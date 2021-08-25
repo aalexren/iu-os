1c5fb8a038c52722912b5f17985298cf75d6d089
Rodion <chernitca@ya.ru>
2021-08-25 17:19:26 +0300

    .gitignore fix

--- week02/ex1.c
+++ week02/ex1.c
@@ -7,9 +7,9 @@ int main() {
    float flo_var = FLT_MAX;
    double dou_var = DBL_MAX;
 
-   printf("Maximum integer: %d\n", int_var);
-   printf("Maximum float: %f\n", flo_var);
-   printf("Maximum double: %f\n", dou_var);
+   printf("Maximum integer – size: %ld, value: %d\n", sizeof(int), int_var);
+   printf("Maximum float – size: %ld, value: %g\n", sizeof(float), flo_var);
+   printf("Maximum double – size %ld, value: %g\n", sizeof(double), dou_var);
 
    return 0;
 }
